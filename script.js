5cd0961862ff2176bbb84095f8e0e6739f59ca52
Renan Santos Gomes <contato@renansgomes.com.br>
2019-10-04 00:12:06 -0300

    feat: Melhoria nos logs e tratamento de erro

--- script.js
+++ script.js
@@ -12,16 +12,29 @@ con.connect(function(err){
     if (err) throw err;
     const sql = "DELETE FROM test LIMIT 10";
     let intervalId = setInterval(() => {
-        con.query(sql, function (err, result) {
-            if (err) throw err;
+        con.query({
+            sql: sql,
+            timeout: 1500,
+        }, function (err, result) {
+            try {
+                if (err) throw err;
+            } catch (e) {
+                console.error('erro' + e);
+                process.exit();
+            }
             console.log("Number of records deleted: " + result.affectedRows);
             log("Number of records deleted: " + result.affectedRows, './logs/script.log');
             if (result.affectedRows === 0) {
                 console.log('Finished!');
                 log('Finished!', './logs/script.log');
+                process.exit();
                 clearInterval(intervalId);
             }
         });
     }, 2000);
-    
 });
+
+function ScriptException(err) {
+    this.value = err;
+    this.message = err.message;
+}
